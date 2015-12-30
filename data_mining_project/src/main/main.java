8775d25ecdf45b6c7a7d8caab83bde235e1a7dea
thibault <thibault.blanc@yahoo.fr>
2015-12-30 12:12:51 +0100

    implementation GUI

--- data_mining_project/src/main/main.java
+++ data_mining_project/src/main/main.java
@@ -6,6 +6,9 @@ import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 import search.search;
 
 public class main {
@@ -20,8 +23,21 @@ public class main {
 		String path="Doc/lemmatisation";
 		Index index=new Index(path);
 		System.out.println(index.identifiantFichier(new File(path)));
-		HashMap<Integer, Double> testSearch=search.searchTerm(index, "avocat syndicat", path);
+		
+		String requete=JOptionPane.showInputDialog ("Rentrez votre requête ici");
+		//"avocat syndicat"
+
+		HashMap<Integer, Double> testSearch=search.searchTerm(index, requete, path);
 		System.out.println(testSearch);
+		final JFrame frame = new JFrame();
+
+		// Release the window and quit the application when it has been closed
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		JOptionPane.showMessageDialog(frame, testSearch);
+
+
 	}
+	
+	
 
 }
