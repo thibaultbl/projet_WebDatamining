987726ee8611c65fc07a06c7d4a35f4788fcfad8
jbqueyrie <jbqueyrie@gmail.com>
2016-01-13 16:18:34 +0100

    Fonction update index avec nouveaux documents

--- data_mining_project/src/index/Index.java
+++ data_mining_project/src/index/Index.java
@@ -8,6 +8,7 @@ import java.io.InputStreamReader;
 import java.text.Normalizer;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 import javax.swing.JOptionPane;
@@ -23,10 +24,13 @@ public class Index {
 	private ArrayList<Noeud> debutTerme;
 	//liste contenant la taille de chaque fichier
 	private static HashMap<Integer, Integer> sizeFile=new HashMap<Integer, Integer>();
+	//liste des noms de fichiers (utile pour l'update de l'index)
+	private static Set<File> listFiles;
 
 	public Index(String path) throws IOException {
 		termTooFrequent frequent=new termTooFrequent(path, 1200, 100);
 		debutTerme=this.InitialiserIndex(path, frequent);
+		listFiles = this.listeFichiers(path);
 
 		/*for(int i=0; i<frequent.getFrequentTerm().size();i++){
 			this.deleteTerm(frequent.getFrequentTerm().get(i));
@@ -41,6 +45,26 @@ public class Index {
 	@Override
 	public String toString() {
 		return "debut terme :"+debutTerme;
+	}
+
+	/**
+	 * Fonction qui récupère les noms de fichiers d'un répertoire
+	 * @param folder
+	 * @return
+	 */
+
+	@SuppressWarnings("null")
+	public static Set<File> listeFichiers(String path) {
+
+		File folder = new File(path);
+		Set<File> files = null;
+
+		for (final File entry : folder.listFiles()) {
+			if (entry.isDirectory()==false) {
+				files.add(entry.getAbsoluteFile());
+			}
+		}
+		return files;
 	}
 
 
@@ -172,15 +196,144 @@ public class Index {
 							}
 						}
 					}
-
-
 				}
 			}
 			sizeFile.put(id.get(f.getName()), positionLigne);
 		}
-
 		return result;
 	}
+
+	/**
+	 * Pour ajouter de nouveaux documents à l'index initial
+	 * @throws IOException
+	 */
+	public static void nvDocIndex(String path, Set<File> listF, termTooFrequent frequent) throws IOException {
+
+		ArrayList<Noeud> result=new ArrayList<Noeud>();
+
+		char[] decomposition;
+		int trouve;
+		int j;
+		int positionLigne;
+		ArrayList<Integer> tempPosition;
+		Noeud temp;
+
+		/*
+		 * Pour ajouter de nouveaux documents à l'index :
+		 * 1 - Récupérer l'index initial
+		 * 2 - Définir le chemin pour accéder aux nouveaux documents
+		 * 2bis - Si les documents ne sont pas déjà inclus dans l'index, on les ajoute. Sinon, on passe au suivant
+		 * 3 - MaJer l'index
+		 * 
+		 */
+
+		//Listage de tous les fichiers dans le répertoire
+		File folder = new File(path);
+		File[] filesInDir = folder.listFiles();
+		HashBiMap<String, Integer> id = identifiantFichier(folder);
+
+		for (final File f : filesInDir) {
+			if (f.isDirectory()==false) {
+				if (listF.contains(f.getAbsoluteFile())) {
+					//Do nothing
+				} else {
+					positionLigne=0;
+
+					{
+						BufferedReader entree = new BufferedReader(new InputStreamReader(new FileInputStream(f)));
+						String ligne;
+						StringTokenizer st;
+						String mot;
+						int nbOcc;
+						//pour chaque ligne dans lématisation
+						while ((ligne = entree.readLine()) != null)
+						{
+							positionLigne++;
+							//On récupére le mot lématiser
+							String text[]= ligne.split("\t");
+							mot=text[2];
+
+							//on le normalise à nouveau (étape inutile si la lématisation à été faite correctement)
+							mot=normalize(mot);		
+
+							if(!frequent.getFrequentTerm().contains(mot)){
+								//on décompose le mot
+								decomposition=mot.toCharArray();
+
+								//on regarde si le noeud correspondant à la première lettre du mot existe déja
+								if(decomposition.length>0){
+
+									trouve=-1;
+									for(int i=0;i<result.size();i++){
+										if(result.get(i).getLettre()==decomposition[0]){
+											trouve=i;
+										}
+									}
+									//Si il n'existe pas on le crée
+									if(trouve ==-1){
+										result.add(new NoeudNonTerminal(decomposition[0], null));
+										trouve=result.size()-1;
+									}
+									j=1;
+									temp=result.get(trouve);
+									//jusqu'à la fin du mot
+									while(j<decomposition.length){
+
+										trouve=-1;
+										if(temp.getNoeudsFils()!=null){
+											for(int i=0;i<temp.getNoeudsFils().size();i++){
+												if(temp.getNoeudsFils().get(i).getLettre()==decomposition[j]){
+													trouve=i;
+												}
+											}
+										}
+										//Si il n'existe pas on le crée
+										if(trouve ==-1){
+											temp.getNoeudsFils().add(new NoeudNonTerminal(decomposition[j], temp));
+											trouve=temp.getNoeudsFils().size()-1;
+										}
+										temp=temp.getNoeudsFils().get(trouve);
+										j++;
+									}
+
+									//On crée le noeud terminal
+									trouve=-1;
+									for(int i=0; i<temp.getNoeudsFils().size();i++){
+										if(temp.getNoeudsFils().get(i) instanceof NoeudTerminal){
+											trouve=i;
+										}
+									}
+
+									if(trouve==-1){
+										temp.getNoeudsFils().add(new NoeudTerminal(temp));
+										trouve=temp.getNoeudsFils().size()-1;
+										temp=temp.getNoeudsFils().get(trouve);
+
+									}
+									else{
+										temp=temp.getNoeudsFils().get(trouve);
+									}
+									//On modifie les éléemnts ud noeud terminal
+									((NoeudTerminal)temp).setFrequenceCorpus((((NoeudTerminal)temp).getFrequenceCorpus())+1);
+									if(((NoeudTerminal)temp).getIndexPositions().containsKey(id.get(f.getName()))){
+										((NoeudTerminal)temp).getIndexPositions().get(id.get(f.getName())).add(Integer.valueOf(positionLigne));
+									}
+									else{
+										tempPosition=new ArrayList<Integer>();
+										tempPosition.add(positionLigne);
+										((NoeudTerminal)temp).getIndexPositions().put(id.get(f.getName()),tempPosition);
+									}
+								}
+							}
+						}
+					}
+					listFiles.add(f.getAbsoluteFile());
+					sizeFile.put(id.get(f.getName()), positionLigne);
+				}
+			}
+		}
+	}
+
 
 	/**
 	 * Pour supprimer un terme dans l'index
